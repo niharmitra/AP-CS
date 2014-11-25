6c10350c6195227109c1ff1aef791a66952cccb3
niharmitra <nihar.roy.mitra@gmail.com>
2014-11-25 13:04:49 -0800

    Redid Mapcar because of failed push

--- RandomExamples/src/MapCar.java
+++ RandomExamples/src/MapCar.java
@@ -2,31 +2,17 @@ import java.util.*;
 import java.util.concurrent.Callable;
 
 
-public class MapCar {
-
-	public static void main(String[] args) {
-		
-	}
+public abstract class MapCar<T, E> {
 	
-	public <V, E> Collection<V> mapcar(Collection<E> list, Callable<V> method) {
-		Collection<V> retVal = null;
-		try {
-			retVal = (Collection<V>)list.getClass().newInstance();
-			for(E elephant : list) {
-				try {
-					retVal.add(method.call());
-				} catch (Exception e) {
-					System.out.println("Couldn't call method qq");
-					e.printStackTrace();
-				}
-			}
-			return retVal;
-		} catch (InstantiationException | IllegalAccessException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+	public ArrayList<T> mapcar(ArrayList<E> list) {
+		ArrayList<T> retVal = new ArrayList<T>();
+		
+		for(E element: list) {
+			retVal.add(method(element));
 		}
+		
 		return retVal;
 	}
 	
-	
+	public abstract T method(E input);
 }
