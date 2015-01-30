4d50bf7f35d8e7087b4e0c1f537654b22d515e25
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-29 17:56:32 -0800

    Update Vocab4.java

--- RandomExamples/src/vocab/Vocab4.java
+++ RandomExamples/src/vocab/Vocab4.java
@@ -5,9 +5,15 @@
  */
 public class Vocab4 {
 	//Strings can be created with a string literal or the String constructor
+	//Strings are a wrapper for arrays of characters
 	String str = "String";
 	str = new String("String");
+	//Collections can hold any types of objects and have different features
+	//For example, arraylists are a collection that allow duplicates
+	ArrayList<String> strs = new ArrayList<String>();
+	
 	public static void main() {
-		
+		strs.add(str);
+		strs.add(str);
 	}
 }
