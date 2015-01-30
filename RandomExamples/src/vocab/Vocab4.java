53372d358a1ff2ce4d03842bad2664ec78ad5892
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-29 17:12:03 -0800

    Basics added

---
+++ RandomExamples/src/vocab/Vocab4.java
@@ -0,0 +1,13 @@
+/*
+ * String
+ * Collection
+ * Graph
+ */
+public class Vocab4 {
+	//Strings can be created with a string literal or the String constructor
+	String str = "String";
+	str = new String("String");
+	public static void main() {
+		
+	}
+}
