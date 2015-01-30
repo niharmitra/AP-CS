248ef9200ec33835c91344efa0ed2a09e48b88d5
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-29 19:41:16 -0800

    Added in info for graph

--- RandomExamples/src/vocab/Vocab4.java
+++ RandomExamples/src/vocab/Vocab4.java
@@ -3,6 +3,10 @@
  * Collection
  * Graph
  */
+ /*
+  * Note that this code doesn't compile
+  * Because I haven't actually included an implementation of BST
+  */
 public class Vocab4 {
 	//Strings can be created with a string literal or the String constructor
 	//Strings are a wrapper for arrays of characters
@@ -15,5 +19,10 @@ public class Vocab4 {
 	public static void main() {
 		strs.add(str);
 		strs.add(str);
+		//A graph is composed of nodes (elements) and edges (the connections between nodes)
+		//For example, if you had a BST
+		BST graph = new BST();
+		//If you could confirm that an edge exists between two nodes in a BST, this would tell you
+		//That one is less than the other.
 	}
 }
