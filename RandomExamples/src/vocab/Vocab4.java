7b857d5ad5d962acedbc50ec192c74076d501a90
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-31 16:17:34 -0800

    Fixing compile errors in vocab asssignment

--- RandomExamples/src/vocab/Vocab4.java
+++ RandomExamples/src/vocab/Vocab4.java
@@ -1,3 +1,7 @@
+package vocab;
+
+import java.util.ArrayList;
+
 /*
  * String
  * Collection
@@ -10,18 +14,18 @@
 public class Vocab4 {
 	//Strings can be created with a string literal or the String constructor
 	//Strings are a wrapper for arrays of characters
-	String str = "String";
-	str = new String("String");
+	private static String str = new String("String"),
+			str2 = "String";
 	//Collections can hold any types of objects and have different features
 	//For example, arraylists are a collection that allow duplicates
-	ArrayList<String> strs = new ArrayList<String>();
+	static ArrayList<String> strs = new ArrayList<String>();
 	
 	public static void main() {
 		strs.add(str);
-		strs.add(str);
+		strs.add(str2);
 		//A graph is composed of nodes (elements) and edges (the connections between nodes)
 		//For example, if you had a BST
-		BST graph = new BST();
+//		BST graph = new BST();
 		//If you could confirm that an edge exists between two nodes in a BST, this would tell you
 		//That one is less than the other.
 	}
