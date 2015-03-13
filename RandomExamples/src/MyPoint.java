be70cb73805b3b240985e0a32a0e5ac84adeab5d
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-03-12 22:54:21 -0700

    MyPoint activity done

---
+++ RandomExamples/src/MyPoint.java
@@ -0,0 +1,21 @@
+
+public class MyPoint implements MyPointInterface {
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+}
---
+++ RandomExamples/src/MyPointInterface.java
@@ -0,0 +1,7 @@
+
+public interface MyPointInterface {
+	public int getX();
+	public int getY();
+	public void setX(int x);
+	public void setY(int y);
+}
