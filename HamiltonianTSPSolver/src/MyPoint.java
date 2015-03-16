b5485e4bedc69a336197604f30d28c51f8ae30cc
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-03-16 14:40:01 -0700

    Update mypoint class

--- HamiltonianTSPSolver/src/MyPoint.java
+++ HamiltonianTSPSolver/src/MyPoint.java
@@ -3,19 +3,26 @@ public class MyPoint implements MyPointInterface {
 	private int x;
 	private int y;
 	
+	//Returns the x coordinate
 	public int getX() {
 		return x;
 	}
-	
+	//Returns the y coordinate
 	public int getY() {
 		return y;
 	}
-	
+	//
 	public void setX(int x) {
 		this.x = x;
 	}
 	
 	public void setY(int y) {
 		this.y = y;
+	}
+	
+	//Returns the distance between two points
+	public double distance(MyPoint other) {
+		return Math.sqrt(Math.pow(this.x - other.getX(), 2)
+				+ Math.pow(this.y - other.getY(), 2));
 	}
 }
--- HamiltonianTSPSolver/src/Solver.java
+++ HamiltonianTSPSolver/src/Solver.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Solver {
 	public static ArrayList<MyPoint> pathfinder(ArrayList<MyPoint> graph) {
 		ArrayList<MyPoint> path = new ArrayList<MyPoint>();
+		//The TSP path is a hamiltonian path
 		path = TSPOracle.tsp(graph);
 		return path;
 	}
